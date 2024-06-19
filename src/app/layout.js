cb303b2f8d15730cfabeac47425355508aa22f9d
Asha Saini <59530575+asha-saini06@users.noreply.github.com>
2024-06-19 23:59:29 +0530

    Responsive Navbar Design

---
+++ src/app/layout.js
@@ -0,0 +1,27 @@
+import Navbar from '@/components/navbar/Navbar'
+import './globals.css'
+import { Inter } from 'next/font/google'
+import Footer from '@/components/footer/Footer'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata = {
+  title: 'WordSmith',
+  description: 'Your platform for literary prowess',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <div className="container">
+          <div className="wrapper">
+            <Navbar />
+            {children}
+            <Footer />
+          </div>
+        </div>
+      </body>
+    </html>
+  )
+}
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import styles from "./navbar.module.css";
+import Image from "next/image";
+import Link from "next/link";
+import AuthLinks from "../authLinks/AuthLinks";
+import ThemeToggle from "../themeToggle/ThemeToggle";
+
+const Navbar = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.social}>
+        <Image src="/facebook.png" alt="facebook" width={20} height={20} />
+        <Image src="/instagram.png" alt="instagram" width={20} height={20} />
+        <Image src="/tiktok.png" alt="tiktok" width={20} height={20} />
+        <Image src="/youtube.png" alt="youtube" width={20} height={20} />
+      </div>
+      <div className={styles.logo}>WordSmith</div>
+      <div className={styles.links}>
+        <ThemeToggle />
+        <Link href="/">HomePage</Link>
+        <Link href="/">Contact</Link>
+        <Link href="/">About</Link>
+        <AuthLinks />
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
