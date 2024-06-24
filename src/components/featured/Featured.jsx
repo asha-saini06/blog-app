7fdd875595f0042cc2f53de77cf944f260034550
Asha Saini <59530575+asha-saini06@users.noreply.github.com>
2024-06-25 00:18:08 +0530

    Featured Post Design

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -1,8 +1,32 @@
 import React from "react";
 import styles from "./featured.module.css";
+import Image from "next/image";
 
 const Featured = () => {
-  return <div className={styles.container}>Featured</div>;
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>
+        <b> Welcome to WordSmith! </b> Where Your Words
+        Find a Home.
+      </h1>
+      <div className={styles.post}>
+        <div className={styles.imgContainer}>
+          <Image src="/p1.jpeg" className={styles.image} alt="image" fill />
+        </div>
+        <div className={styles.textContainer}>
+          <h1 className={styles.postTitle}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </h1>
+          <p className={styles.postDesc}>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Esse
+            facilis saepe porro provident reprehenderit maiores a dignissimos
+            quisquam tenetur vero!
+          </p>
+          <button className={styles.button}>Read More</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Featured;
