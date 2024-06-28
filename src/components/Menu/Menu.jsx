65e3a7efb204782719e65bbfae1c6e76bb3a4de0
Asha Saini <59530575+asha-saini06@users.noreply.github.com>
2024-06-29 01:27:06 +0530

    Side menu section & Pagination Button Design

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -3,6 +3,7 @@ import styles from "./menu.module.css";
 import Link from "next/link";
 import Image from "next/image";
 import MenuPosts from "../menuPosts/menuPosts";
+import MenuCategories from "../menuCategories/MenuCategories";
 
 const Menu = () => {
   return (
@@ -13,41 +14,7 @@ const Menu = () => {
 
       <h2 className={styles.subtitle}>Discover by topic</h2>
       <h1 className={styles.title}>Categories</h1>
-      <div className={styles.categoryList}>
-        <Link
-          href="/blog?cat=style"
-          className={`${styles.categoryItem} ${styles.style}`}
-        >
-          Style
-        </Link>
-        <Link
-          href="/blog"
-          className={`${styles.categoryItem} ${styles.fashion}`}
-        >
-          Fashion
-        </Link>
-        <Link href="/blog" className={`${styles.categoryItem} ${styles.food}`}>
-          Food
-        </Link>
-        <Link
-          href="/blog"
-          className={`${styles.categoryItem} ${styles.travel}`}
-        >
-          Travel
-        </Link>
-        <Link
-          href="/blog"
-          className={`${styles.categoryItem} ${styles.culture}`}
-        >
-          Culture
-        </Link>
-        <Link
-          href="/blog"
-          className={`${styles.categoryItem} ${styles.coding}`}
-        >
-          Coding
-        </Link>
-      </div>
+      <MenuCategories />
 
       <h2 className={styles.subtitle}>Chosen by the editor</h2>
       <h1 className={styles.title}>Editor&apos;s Pick</h1>
