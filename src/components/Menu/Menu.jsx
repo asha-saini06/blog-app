0ae1d7cb7fa917059947a8533b3d7c0e6eb9ee8c
Asha Saini <59530575+asha-saini06@users.noreply.github.com>
2024-06-28 23:58:51 +0530

    Side Menu Component Design

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,8 +1,59 @@
 import React from "react";
 import styles from "./menu.module.css";
+import Link from "next/link";
+import Image from "next/image";
+import MenuPosts from "../menuPosts/menuPosts";
 
 const Menu = () => {
-  return <div className={styles.container}>Menu</div>;
+  return (
+    <div className={styles.container}>
+      <h2 className={styles.subtitle}>{"What's hot"}</h2>
+      <h1 className={styles.title}>Most Popular</h1>
+      <MenuPosts withImage={false} />
+
+      <h2 className={styles.subtitle}>Discover by topic</h2>
+      <h1 className={styles.title}>Categories</h1>
+      <div className={styles.categoryList}>
+        <Link
+          href="/blog?cat=style"
+          className={`${styles.categoryItem} ${styles.style}`}
+        >
+          Style
+        </Link>
+        <Link
+          href="/blog"
+          className={`${styles.categoryItem} ${styles.fashion}`}
+        >
+          Fashion
+        </Link>
+        <Link href="/blog" className={`${styles.categoryItem} ${styles.food}`}>
+          Food
+        </Link>
+        <Link
+          href="/blog"
+          className={`${styles.categoryItem} ${styles.travel}`}
+        >
+          Travel
+        </Link>
+        <Link
+          href="/blog"
+          className={`${styles.categoryItem} ${styles.culture}`}
+        >
+          Culture
+        </Link>
+        <Link
+          href="/blog"
+          className={`${styles.categoryItem} ${styles.coding}`}
+        >
+          Coding
+        </Link>
+      </div>
+
+      <h2 className={styles.subtitle}>Chosen by the editor</h2>
+      <h1 className={styles.title}>Editor&apos;s Pick</h1>
+      <MenuPosts withImage={true} />
+    </div>
+  );
 };
 
 export default Menu;
