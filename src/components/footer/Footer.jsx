3d7ea73f56a1e3236a53b38615515f6968eb67aa
Asha Saini <59530575+asha-saini06@users.noreply.github.com>
2024-06-29 21:56:54 +0530

    Footer Component Design

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,53 @@
 import React from "react";
 import styles from "./footer.module.css";
+import Image from "next/image";
+import Link from "next/link";
 
 const Footer = () => {
-  return <div className={styles.container}>Footer</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.info}>
+        <div className={styles.logo}>
+          <Image src="/logo.png" alt="logo" width={50} height={50} />
+          <h1 className={styles.logoText}>WordSmith</h1>
+        </div>
+        <p className={styles.desc}>
+          Discover WordSmith, your gateway to effortless blogging. Start writing
+          your blog, connect with like-minded creators, and explore diverse
+          narratives.
+        </p>
+        <div className={styles.icons}>
+          <Image src="/facebook.png" alt="facebook" width={18} height={18} />
+          <Image src="/instagram.png" alt="instagram" width={18} height={18} />
+          <Image src="/tiktok.png" alt="tiktok" width={18} height={18} />
+          <Image src="/youtube.png" alt="youtube" width={18} height={18} />
+        </div>
+      </div>
+      <div className={styles.links}>
+        <div className={styles.list}>
+          <span className={styles.listTitle}>Links</span>
+          <Link href="/">Homepage</Link>
+          <Link href="/">Blog</Link>
+          <Link href="/">About</Link>
+          <Link href="/">Contact</Link>
+        </div>
+        <div className={styles.list}>
+          <span className={styles.listTitle}>Tags</span>
+          <Link href="/">Style</Link>
+          <Link href="/">Fashion</Link>
+          <Link href="/">Coding</Link>
+          <Link href="/">Travel</Link>
+        </div>
+        <div className={styles.list}>
+          <span className={styles.listTitle}>Social</span>
+          <Link href="/">Facebook</Link>
+          <Link href="/">Instagram</Link>
+          <Link href="/">Tiktok</Link>
+          <Link href="/">YouTube</Link>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
